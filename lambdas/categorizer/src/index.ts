afeeebebe471f804e4b43d54870289be712cbfc9
Heshan <heshanpun@gmail.com>
2026-03-10 21:23:21 -0600

    lambda docs

--- lambdas/categorizer/src/index.ts
+++ lambdas/categorizer/src/index.ts
@@ -1,3 +1,4 @@
+// Categorizer: string-match rules against descriptions, tag unmatched as uncategorized.
 import { Handler } from 'aws-lambda';
 
 export const handler: Handler = async (event) => {
--- lambdas/report-maker/src/index.ts
+++ lambdas/report-maker/src/index.ts
@@ -1,3 +1,4 @@
+// Report Maker: aggregate monthly transactions by category, include uncategorized section.
 import { Handler } from 'aws-lambda';
 
 export const handler: Handler = async (event) => {

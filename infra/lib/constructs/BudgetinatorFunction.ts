6a78c8330b8c05f49d27d5432dc54b6154b6a7a1
Heshan <heshanpun@gmail.com>
2026-03-18 21:34:44 -0600

    feat: add BudgetinatorFunction class for AWS Lambda integration

---
+++ infra/lib/constructs/BudgetinatorFunction.ts
@@ -0,0 +1,12 @@
+import { Construct } from 'constructs';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { appNamePrefix } from '../../constants';
+
+export class BudgetinatorFunction extends NodejsFunction {
+  constructor(scope: Construct, id: string, props: NodejsFunctionProps) {
+    super(scope, id, {
+      functionName: `${appNamePrefix}${id}`,
+      ...props,
+    });
+  }
+}
